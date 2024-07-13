ea6c2a8a4cb8d473f5564b249fb54b1b0caf72c7
Yones Maheri <yonesmaheri80@gmail.com>
2024-07-13 12:53:43 +0330

    add dashboard page

---
+++ components/templates/dashboardPage/chartData.js
@@ -0,0 +1,62 @@
+export const Data = [
+  {
+    id: 1,
+    month: "فروردین",
+    userGain: 2587,
+  },
+  {
+    id: 2,
+    month: "اردیبهشت",
+    userGain: 4258,
+  },
+  {
+    id: 3,
+    month: "خرداد",
+    userGain: 9054,
+  },
+  {
+    id: 4,
+    month: "تیر",
+    userGain: 8328,
+  },
+  {
+    id: 5,
+    month: "مرداد",
+    userGain: 7824,
+  },
+  {
+    id: 6,
+    month: "شهریور",
+    userGain: 5789,
+  },
+  {
+    id: 7,
+    month: "مهر",
+    userGain: 3786,
+  },
+  {
+    id: 8,
+    month: "آبان",
+    userGain: 4865,
+  },
+  {
+    id: 9,
+    month: "آذر",
+    userGain: 1258,
+  },
+  {
+    id: 10,
+    month: "دی",
+    userGain: 9683,
+  },
+  {
+    id: 11,
+    month: "بهمن",
+    userGain: 10257,
+  },
+  {
+    id: 12,
+    month: "اسفند",
+    userGain: 5976,
+  }
+];
